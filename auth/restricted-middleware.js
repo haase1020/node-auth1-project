46dd57240ca0fdb5adf4c75b9e0afef2472c55d1
Mandi Haase <haase1020@gmail.com>
2020-03-23 20:23:22 -0400

    trying to fix bugs

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,7 +1,8 @@
-module.exports = (req, res,next) => {
-    iff(req.session && req.session.user) {
-        next();
+module.exports = (req, res, next) => {
+    
+    if (req.session && req.session.user) {
+      next();
     } else {
-        res.status(401).json({ "you can go no further" });
+      res.status(401).json({ you: "shall not pass!" });
     }
-};
+  };
